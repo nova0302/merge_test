89b45f2c5abe33935e1ef3cd62e44d0f6226640e
SangaleKim <nova0302@hotmail.com>
2024-01-28 15:08:20 +0900

    for loop added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+
+const int N = 4;
 
 int main(int argc, char *argv[])
 {
 
+  for (int i=0; i < N; ++i)
+      cout << i << " : Hello World!!" << endl;
   
   return 0;
 }
