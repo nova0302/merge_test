00f90894c340c0da8b67fe7aea06d43332ec9623
Sangale Kim <nova0302@hotmail.com>
2024-01-29 23:43:10 +0900

    IObserver added

--- main.cpp
+++ main.cpp
@@ -2,23 +2,26 @@
 
 using namespace std;
 
-class ISubject
-{
+class IObserver {
 public:
-  ISubject();
-  virtual ~ISubject();
+  virtual ~IObserver(){}
+  virtual void Update(const std::string& message_from_subject) = 0;
 };
 
-
+class ISubject {
+public:
+  virtual ~ISubject(){}
+  virtual void Attach(IObserver* o) = 0;
+  virtual void Detach(IObserver* o) = 0;
+  virtual void Notify() = 0;
+};
 
 const int N = 32;
-
 
 int main(int argc, char *argv[])
 {
 
   for (int i=0; i < N; ++i)
       cout << i << " : Hello World!!" << endl;
-  
   return 0;
 }
