22ceed1490658c7b350e4cc978782eb9abe5eb8e
Sangale Kim <nova0302@hotmail.com>
2024-01-29 23:57:00 +0900

    Observer added

--- main.cpp
+++ main.cpp
@@ -43,6 +43,35 @@ private:
   std::string message_;
 };
 
+class Observer : public IObserver {
+ public:
+  Observer(Subject &subject) : subject_(subject) {
+    this->subject_.Attach(this);
+    std::cout << "Hi, I'm the Observer \"" << ++Observer::static_number_ << "\".\n";
+    this->number_ = Observer::static_number_;
+  }
+  virtual ~Observer() {
+    std::cout << "Goodbye, I was the Observer \"" << this->number_ << "\".\n";
+  }
+
+  void Update(const std::string &message_from_subject) override {
+    message_from_subject_ = message_from_subject;
+    PrintInfo();
+  }
+  void RemoveMeFromTheList() {
+    subject_.Detach(this);
+    std::cout << "Observer \"" << number_ << "\" removed from the list.\n";
+  }
+  void PrintInfo() {
+    std::cout << "Observer \"" << this->number_ << "\": a new message is available --> " << this->message_from_subject_ << "\n";
+  }
+
+ private:
+  std::string message_from_subject_;
+  Subject &subject_;
+  static int static_number_;
+  int number_;
+};
 
 const int N = 32;
 
