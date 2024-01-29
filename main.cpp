29e9a6c6c87df520b8579c64738d6a03c27f70a1
Sangale Kim <nova0302@hotmail.com>
2024-01-29 23:52:31 +0900

    Subject added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 
 using namespace std;
 
@@ -16,12 +17,39 @@ public:
   virtual void Notify() = 0;
 };
 
+class Subject : public ISubject {
+public:
+  virtual ~Subject(){std::cout << "Goodbye, I was the Subject.\n";}
+  void Attach(IObserver *observer) override {list_observer_.push_back(observer);}
+  void Detach(IObserver *observer) override {list_observer_.remove(observer);}
+  void Notify() override {
+    std::list<IObserver *>::iterator iterator = list_observer_.begin();
+    HowManyObserver();
+    while (iterator != list_observer_.end()) {
+      (*iterator)->Update(message_);
+      ++iterator;
+    }
+  }
+
+  void CreateMessage(std::string message = "Empty") {
+    this->message_ = message;
+    Notify();
+  }
+  void HowManyObserver() {
+    std::cout << "There are " << list_observer_.size() << " observers in the list.\n";
+  }
+private:
+  std::list<IObserver *> list_observer_;
+  std::string message_;
+};
+
+
 const int N = 32;
 
 int main(int argc, char *argv[])
 {
 
   for (int i=0; i < N; ++i)
-      cout << i << " : Hello World!!" << endl;
+    cout << i << " : Hello World!!" << endl;
   return 0;
 }
